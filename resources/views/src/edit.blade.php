479688ec60bbe3497462f93e01a8952f96289011
RK.120 <rk@maxx-marketing.net>
2019-05-23 18:06:01 +0300

    .

--- resources/views/src/edit.blade.php
+++ resources/views/src/edit.blade.php
@@ -1,43 +1,43 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <form action="{{ $item->id ? route($branch . '.update', $item->id) : route($branch . '.store') }}" method="POST">
-        <div class="form-group row">
-            <div class="header col-sm">
-                <h1>@lang('app.src') - @lang('app.' . ($item->id ? 'edit' : 'create'))</h1>
+    <div class="container">
+        <form action="{{ $item->id ? route($branch . '.update', $item->id) : route($branch . '.store') }}" method="POST">
+            <div class="form-group row">
+                <div class="header col-sm">
+                    <h1>@lang('app.src') - @lang('app.' . ($item->id ? 'edit' : 'create'))</h1>
+                </div>
+                <div class="btns col-sm text-sm-right">
+                    <a class="btn btn-primary" href="{{ route($branch . '.index') }}"> Back</a>
+                    <button type="submit" class="btn btn-success">@lang('app.save')</button>
+                </div>
             </div>
-            <div class="btns col-sm text-sm-right">
-                <a class="btn btn-primary" href="{{ route($branch . '.index') }}"> Back</a>
-                <button type="submit" class="btn btn-success">@lang('app.save')</button>
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">@lang('app.name')</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="name" name="name" value="{{ $item->name }}">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="name" class="col-sm-2 col-form-label">@lang('app.name')</label>
-            <div class="col-sm-10">
-                <input class="form-control" id="name" name="name" value="{{ $item->name }}">
+            <div class="form-group row">
+                <label for="alias" class="col-sm-2 col-form-label">@lang('app.alias')</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="alias" name="alias" value="{{ $item->alias }}">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="alias" class="col-sm-2 col-form-label">@lang('app.alias')</label>
-            <div class="col-sm-10">
-                <input class="form-control" id="alias" name="alias" value="{{ $item->alias }}">
+            <div class="form-group row">
+                <label for="url" class="col-sm-2 col-form-label">@lang('app.url')</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="url" name="url" value="{{ $item->url }}">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="url" class="col-sm-2 col-form-label">@lang('app.url')</label>
-            <div class="col-sm-10">
-                <input class="form-control" id="url" name="url" value="{{ $item->url }}">
+            <div class="form-group row">
+                <label for="filter_min_votes" class="col-sm-2 col-form-label">@lang('app.filter_min_votes')</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="filter_min_votes" name="filter_min_votes" value="{{ $item->filter_min_votes }}">
+                </div>
             </div>
-        </div>
-        <div class="form-group row">
-            <label for="filter_min_votes" class="col-sm-2 col-form-label">@lang('app.filter_min_votes')</label>
-            <div class="col-sm-10">
-                <input class="form-control" id="filter_min_votes" name="filter_min_votes" value="{{ $item->filter_min_votes }}">
-            </div>
-        </div>
-        <input name="id" type="hidden" value="{{ $item->id }}">
-        @csrf
-        @method($item->id ? 'PUT' : 'POST')
-    </form>
-</div>
+            <input name="id" type="hidden" value="{{ $item->id }}">
+            @csrf
+            @method($item->id ? 'PUT' : 'POST')
+        </form>
+    </div>
 @endsection
