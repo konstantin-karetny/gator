727c68a3ad4e61bb1c738c89e77da05ef1bbdbba
RK.120 <rk@maxx-marketing.net>
2019-08-14 18:02:08 +0300

    .

--- storage/framework/views/656bd1de5d5e4c40bc0a9cb02445b94a00c6e191.php
+++ storage/framework/views/656bd1de5d5e4c40bc0a9cb02445b94a00c6e191.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link href="<?php echo e(asset('favicon.ico')); ?>" rel="shortcut icon" type="image/vnd.microsoft.icon">
-        <link href="<?php echo e(asset('css/bootstrap.min.css')); ?>" rel="stylesheet">
-        <link href="<?php echo e(asset('css/bootstrap-toggle.min.css')); ?>" rel="stylesheet">
-        <link href="<?php echo e(asset('css/fontawesome.css')); ?>" rel="stylesheet">
-        <link href="<?php echo e(asset('css/reset.css')); ?>" rel="stylesheet">
-        <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+        <link href="/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+        <link href="/css/bootstrap-toggle.min.css" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
         <?php if(file_exists(public_path($css))): ?>
             <link href="<?php echo e(asset($css)); ?>" rel="stylesheet">
         <?php endif; ?>
         <meta charset="UTF-8">
         <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <script src="<?php echo e(asset('js/jquery-3.4.1.min.js')); ?>"></script>
-        <script src="<?php echo e(asset('js/bootstrap-popper.min.js')); ?>"></script>
-        <script src="<?php echo e(asset('js/bootstrap.min.js')); ?>"></script>
-        <script src="<?php echo e(asset('js/bootstrap-toggle.min.js')); ?>"></script>
-        <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+        <script src="/js/app.js"></script>
+        <script src="/js/bootstrap-toggle.min.js"></script>
         <?php if(file_exists(public_path($js))): ?>
             <script src="<?php echo e(asset($js)); ?>"></script>
         <?php endif; ?>
@@ -31,6 +25,7 @@
     </head>
     <body class="<?php echo e($class); ?>">
         <?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <example-component></example-component>
         <?php echo $__env->make('layouts.msgs', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
         <?php echo $__env->yieldContent('content'); ?>
     </body>
